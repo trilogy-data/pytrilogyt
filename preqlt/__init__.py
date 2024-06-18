6b97b8cf372b3da79fcdf3361071025dcf05288c
efromvt <ethan.dickinson@gmail.com>
2024-06-17 21:51:03 -0400

    version bump

--- preqlt/__init__.py
+++ preqlt/__init__.py
@@ -2,6 +2,6 @@ from preqlt.core import enrich_environment
 from preqlt.dbt.generate_dbt import generate_model as generate_dbt_model
 from preqlt.dbt.config import DBTConfig
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = ["enrich_environment", "generate_dbt_model", "DBTConfig"]
