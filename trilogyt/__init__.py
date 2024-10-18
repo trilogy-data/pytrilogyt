17a2859485bd9f0b3623e0c63c68a5a77887bad3
efromvt <ethan.dickinson@gmail.com>
2024-10-18 08:23:23 -0400

    version bump

--- trilogyt/__init__.py
+++ trilogyt/__init__.py
@@ -2,6 +2,6 @@ from trilogyt.core import enrich_environment
 from trilogyt.dbt.generate import generate_model as generate_dbt_model
 from trilogyt.dbt.config import DBTConfig
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 __all__ = ["enrich_environment", "generate_dbt_model", "DBTConfig"]
