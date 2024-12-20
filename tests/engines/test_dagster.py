8f504ec9e62062ec1451d0bda67e9a7c5fd3d1ab
efromvt <ethan.dickinson@gmail.com.com>
2024-12-19 21:41:30 -0500

    universal path for test

--- tests/engines/test_dagster.py
+++ tests/engines/test_dagster.py
@@ -7,9 +7,9 @@ def test_model_input():
     model_input = ModelInput(
         name="test",
         file_path="test",
-        import_path=Path(
-            "assets\optimization\ds445831a9e9e484d7f90c672304367319328b5fd8d8093cc8a12c9d788f4e4bd7_gen_model.py"
-        ),
+        import_path=Path("assets")
+        / "optimization"
+        / "ds445831a9e9e484d7f90c672304367319328b5fd8d8093cc8a12c9d788f4e4bd7_gen_model.py",
     )
     assert (
         model_input.python_import
