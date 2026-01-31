55eb3cc27b5ba377521e123657c259db0c5c752f
efromvt <ethan.dickinson@gmail.com>
2026-01-31 11:00:00 -0500

    fix rendering

--- tests/integration/dbt/models/customer_three/dim_splits_three_gen_model.sql
+++ tests/integration/dbt/models/customer_three/dim_splits_three_gen_model.sql
@@ -2,8 +2,6 @@
 -- Do not edit manually
 {{ config(materialized='table') }}
 SELECT
-    "dscte_generic_scalar_02e41b09"."cte_generic_split" as "cte_generic_split"
+    "dscte_generic_split_4a0c66ea"."cte_generic_split" as "cte_generic_split"
 FROM
-    {{ ref('dscte_generic_scalar_02e41b09_gen_model') }} as "dscte_generic_scalar_02e41b09"
-GROUP BY 
-    "dscte_generic_scalar_02e41b09"."cte_generic_split"
+    ({{ ref('dscte_generic_split_4a0c66ea_gen_model') }}) as "dscte_generic_split_4a0c66ea"
